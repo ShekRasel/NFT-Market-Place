21e4578a0558a3eb3b3a488f1eeebadb26f13c25
Shek Rasel <128912090+ShekRasel@users.noreply.github.com>
2024-09-11 16:09:37 +0600

    routing for the home page is done

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,27 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from './pages/Home.jsx'
+import Layout from './Layout.jsx'
 
+let Routes = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout/>,
+    children: [
+      {path: '/',
+        element: <App/>
+      },
+      {
+        path:'home',
+        element: <Home/>
+      }
+    ],
+  }
+])
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+      <RouterProvider router={Routes}/>
   </StrictMode>,
 )
