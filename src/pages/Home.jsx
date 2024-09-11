2a5acf7641a6daa45087402f74587ea29d69d173
Shek Rasel <128912090+ShekRasel@users.noreply.github.com>
2024-09-11 16:10:53 +0600

    home page done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,7 +10,7 @@ function Home() {
     <div className=' bg-gray-900 text-white h-auto pt-44 px-4 md:px-10 xl:px-24 2xl:px-80'>
         <div className='lg:flex justify-between  '>
             <div className=' '>
-                <h1 className='text-3xl md:text-5xl lg:text-4xl font-bold'>
+                <h1 className='text-3xl md:text-5xl lg:text-5xl font-bold'>
                     <p className=''>Tailwind Template for</p>
                     <p className='py-2'>NFT MarketPlace and</p>
                     <p className=''>Web3 Platform</p>
