bbcf1a5c95c9e0b9ebb10c5bcda3389ec7085aaa
Shek Rasel <128912090+ShekRasel@users.noreply.github.com>
2024-09-15 17:54:42 +0600

    added onclick

--- src/components/homePageComponents/MyCarousel.jsx
+++ src/components/homePageComponents/MyCarousel.jsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
 import { FaLongArrowAltLeft, FaLongArrowAltRight } from "react-icons/fa";
 
 function MyCarousel() {
+  const carouselRef = useRef(); // create a ref for the carousel
+
   const artistCard = [
     {
       name: '@David Miller',
@@ -11,17 +13,17 @@ function MyCarousel() {
       photo: '/src/assets/images/image1.jpg',
     },
     {
-      name: '@David Miller',
-      view: '9.75 ETH',
+      name: '@Elon Mask',
+      view: '7.5 ETH',
       photo: '/src/assets/images/image2.jpg',
     },
     {
-      name: '@David Miller',
+      name: '@Hiliya_Farah',
       view: '9.75 ETH',
       photo: '/src/assets/images/image3.jpg',
     },
     {
-      name: '@David Miller',
+      name: '@Willium_Dev',
       view: '9.75 ETH',
       photo: '/src/assets/images/image4.jpg',
     },
@@ -61,23 +63,30 @@ function MyCarousel() {
     <div className='h-auto mt-16'>
       <div className='flex justify-between border-r-0 border-l-0 border-t-0 border pb-6'>
         <h1 className='text-3xl font-semibold'>Top Artists</h1>
-        <span className='flex gap-4'>
-          <span className='p-3 px-4 rounded-md items-center hover:bg-indigo-600 bg-slate-500'>
+        <div className='flex gap-4'>
+          <span
+            className='p-3 px-4 rounded-md items-center hover:bg-indigo-600 bg-slate-500 cursor-pointer'
+            onClick={() => carouselRef.current.previous()} // move to previous slide
+          >
             <FaLongArrowAltLeft />
           </span>
-          <span className='p-3 px-4 rounded-md items-center hover:bg-indigo-600 bg-slate-500'>
+          <span
+            className='p-3 px-4 rounded-md items-center hover:bg-indigo-600 bg-slate-500 cursor-pointer'
+            onClick={() => carouselRef.current.next()} // move to next slide
+          >
             <FaLongArrowAltRight />
           </span>
-        </span>
+        </div>
       </div>
 
       <div className='gap-4 mt-12'>
         <Carousel
+          ref={carouselRef} // assign the ref to Carousel
           responsive={responsive}
           infinite={true}
+          arrows={false}
           autoPlay={true}
-          
-          itemClass="px-2" 
+          itemClass="px-2"
         >
           {artistCard.map((card, index) => (
             <div key={index} className='flex p-6 py-8 rounded-xl border-2 gap-6 items-center text-lg hover:border-pink-500 bg-slate-700'>
